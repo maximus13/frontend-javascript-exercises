668ee11dbedde952a9bc2641064108999fa43bc5
Maximus13 <tlembi@intellirentsf.com>
2015-03-11 15:36:12 -0700

    Passed all tests 02-08 except one in conditionals

--- 02-reading-mocha-tests/00-a-tested-function/challenge.js
+++ 02-reading-mocha-tests/00-a-tested-function/challenge.js
@@ -4,6 +4,3 @@ module.exports.linkTo = function(text, address) {
 
 
 
-// module.exports.linkTo = function(text, address) {   
-//   return "<a href='" + text + "'>" + address + "</a>"
-// };
--- 07-if-statements/00-basics/challenge.js
+++ 07-if-statements/00-basics/challenge.js
@@ -1,5 +1,29 @@
-module.exports.favoriteNumber = undefined;
+module.exports.favoriteNumber = function(favNum,guessNum){
+  if(guessNum > favNum){
+    return "Too high"
+  } else if (guessNum < favNum){
+    return "Too low"
+  } else if (guessNum === favNum)
+  return "You got it!"
+};
 
-module.exports.checkLock = undefined;
 
-module.exports.canIGet = undefined;
+
+module.exports.checkLock = function(numOne,numTwo,numThree,numFour){
+  var range = []
+  
+}
+
+module.exports.canIGet = function(item,money){
+  if(item === "MacBook Air" && money >= 999){
+    return true;
+  } else if (item === "MacBook Pro" && money >= 1299){
+    return true;
+  } else if (item === "Mac Pro" && money >= 2499){
+    return true;
+  } else if (item =="Apple Sticker" && money >= 1){
+    return true;
+  } else {
+    return false;
+  }
+};

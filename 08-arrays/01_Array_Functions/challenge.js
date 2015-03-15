badf5e61ea875c0b2d094a478eb60ede5f7a2b6a
Maximus13 <tlembi@intellirentsf.com>
2015-03-14 23:22:25 -0700

    completed arrays challenges

--- 08-arrays/01_Array_Functions/challenge.js
+++ 08-arrays/01_Array_Functions/challenge.js
@@ -1,10 +1,15 @@
 module.exports.reversePlusOne = function(array){
-array.reverse();
-array.unshift(1);
-return array; 
+  array.reverse();
+  array.unshift(1);
+  return array; 
 };
 //take an array of two numbers, reverse the array, add an element (1) to the start of the array
 
-module.exports.plusesEverywhere = undefined;
+module.exports.plusesEverywhere = function (array) {
+  return array.join('+');
+};
 
-module.exports.arrayQuantityPlusOne = undefined;
+module.exports.arrayQuantityPlusOne = function(array){
+  return array.length +1;
+
+};

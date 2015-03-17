5bed405ec93cd325e9b64a58222fbc1468027962
Maximus13 <tlembi@intellirentsf.com>
2015-03-16 22:23:31 -0700

    completed 09-objects/00-basics

--- 09-objects/00-basics/challenge.js
+++ 09-objects/00-basics/challenge.js
@@ -1,7 +1,35 @@
-module.exports.createCourse = undefined;
+module.exports.createCourse = function (title, duration, students){
+  var course = {
+    title: title,
+    duration: duration,
+    students: students,
+  }
+  return course
+};
 
-module.exports.addProperty = undefined;
+module.exports.addProperty = function (object, newProperty, newValue){
+  if (object[newProperty] === undefined) {
+    object[newProperty] = newValue
+  }
+  return object
+};
 
-module.exports.formLetter = undefined;
 
-module.exports.canIGet = undefined;
+module.exports.formLetter = function(letter) {
+  return "Hello " + letter.recipient + ",\n\n" + letter.msg + "\n\nSincerely,\n" + letter.sender;
+};
+
+module.exports.canIGet = function canIGet(item, money) {
+  return {
+    "MacBook Air": 999,
+    "MacBook Pro": 1299,
+    "Mac Pro": 2499,
+    "Apple Sticker": 1
+  }[item] <= money;
+};
+
+
+
+
+//To solve without IF statements look up block notation
+// http://stackoverflow.com/questions/29067715/stuck-on-objects-in-javascript
